6e6836709d19ffd61931e2ddc3aed0497851767e
Jesús Urrutia <jurrutia@acid.cl>
2019-02-28 15:35:31 -0300

    fix: retry db connection on fail

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { createConnection } from 'typeorm';
+import retry from 'async-retry';
 import routes from './routes';
 
+async function getConnection() {
+  try {
+    return await createConnection();
+  } catch (e) {
+    throw e;
+  }
+}
+
 (async () => {
-  const connection = await createConnection();
+  try {
+    await retry(getConnection);
+  } catch (e) {
+    console.error(e);
+  }
 
   const app = express();
   app.use(bodyParser.json());
